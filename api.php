05da094e8a870a3eb998cb0990f6b2d8656c773e
mattiatealdi <mattiatealdi@gmail.com>
2021-06-09 16:55:22 +0200

    commit

--- api.php
+++ api.php
@@ -1,21 +1,25 @@
 <?php
 
+//include il mio database
 include __DIR__ . "/data/db.php";
 
 $genres = [];
+//controllo se nella select il genere è all in quel caso l'array albums è composto da tutti gli albums disponibili altrimenti è vuoto e lo popolo in seguito
 $albums = empty($_GET['genre']) || $_GET['genre'] === 'all' ? $database : [];
 
-foreach($database as $album){
 
+//compongo un array con tutti i generi musicali
+foreach($database as $album){
+//se il genere è già presente nell'array non lo aggiungo viceversa si
   if(!in_array($album['genre'],$genres)){
     $genres[] = $album['genre'];
   }
 
 };
 
-// aggiungo l'elemento col genere corrispondete solo se l'array $albums è vuoto
+//se l'array di albums è vuoto(ovvero contiene un genere) popolo l'array albums con gli album di quel genere
 if(count($albums) === 0){
-
+//ciclo il mio database per poi andare ad aggiungere l'album con il genere associato ad esso se il genere dell'album corrisponde alla $_GET
   foreach($database as $album){
     if($album['genre'] === $_GET['genre']){
       $albums[] = $album;
@@ -24,14 +28,15 @@ if(count($albums) === 0){
 
 };
 
-
+//passo a json encode due array: uno contenente l'array di albums
+//l'altro contenente tutti i generi
 $response = [
   'albums' => $albums,
   'genres' => $genres
 ];
 
 
-
+//trasformo il mio db in api
 header('Content-Type: application/json');
 
 echo json_encode($response);
--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-
 const app = new Vue({
 
     el: '#app',
     data:{
-  
+
+    //definisco le mie variabili
       albums:[],
-      genres:[],
+      genresAvailibles:[],
+      //la variabile sotto ha un v-model con la select così quanto la passo alla mia chiamata API posso usarla come filtro per i generi
       genreToSearch: 'all',
+      //URL dell'API
       apiURL: 'http://localhost:8888/php-ajax-dischi/api.php'
   
     },
     methods:{
-  
+  //Chiamata API
       getAPI(){
         axios.get(this.apiURL,{
           params:{
+            //passo come parametro il genere selezionato per filtrare le musiche di un certo genere
             genre: this.genreToSearch
           }
         })
         .then(res => {
-  
+        //aggiungo ai miei array gli album cercati e i generi disponibili
           this.albums = res.data.albums;
-          this.genres = res.data.genres;
+          this.genresAvailibles = res.data.genres;
           console.log(this.genres);
           console.log(this.albums);
   
@@ -33,6 +36,7 @@ const app = new Vue({
       }
   
     },
+    //una volta creata la pagina chiamo la mia funzione getAPI
     created(){
       this.getAPI();
     }
